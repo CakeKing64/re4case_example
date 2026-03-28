2997de7da8c48b03b81ab53597bb4aa9379bd6e2
cakeking64 <cakeking64@gmail.com>
2026-03-29 00:33:07 +1000

    Yep

---
+++ lua/autorun/autorun.lua
@@ -0,0 +1,20 @@
+
+
+if SEVER then
+	AddCSLuaFile()
+end
+
+hook.Add("CaseRegisterItems", "MyCustomItem", function ()
+	-- Add hopium from https://steamcommunity.com/sharedfiles/filedetails/?id=3606384767
+	CaseInventory:RegisterItem(
+		CaseWeapon("weapon_hopium", -- Weapon name
+			CaseRenderInfo("models/weapons/w_hopium.mdl",  -- Model to use
+				0.7, -- Model Scale
+				{-90}, -- Rotation
+				Vector(0,-10, -12) -- Render offset
+			),
+			6, -- X Grid Size
+			2 -- Y Grid Size
+		)
+	)
+end)
