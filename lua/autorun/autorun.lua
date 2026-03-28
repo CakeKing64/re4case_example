8b907859923cd22c857e571a6d6c1d2c3a668c93
Geordie <cakeking64@gmail.com>
2026-03-29 01:00:47 +1000

    Show all the axes for rotation

--- lua/autorun/autorun.lua
+++ lua/autorun/autorun.lua
@@ -10,7 +10,7 @@ hook.Add("CaseRegisterItems", "MyCustomItem", function ()
 		CaseWeapon("weapon_hopium", -- Weapon name, you can get this from right clicking stuff in the spawn menu and then hitting copy to clipboard
 			CaseRenderInfo("models/weapons/w_hopium.mdl",  -- Model to use
 				0.7, -- Model Scale
-				{-90}, -- Rotation
+				{-90, 0, 0}, -- Rotation
 				Vector(0,-10, -12) -- Render offset
 			),
 			6, -- X Grid Size
